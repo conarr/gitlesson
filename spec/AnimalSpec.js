54f998a3d5ceeace653a6f1aba24ef6e51cc1081
Conor Arrigan <18707425+conarr@users.noreply.github.com>
2018-02-08 13:52:13 +0000

    Add files via upload

---
+++ spec/AnimalSpec.js
@@ -0,0 +1,34 @@
+describe("Animal Unit Tests", function() {
+
+  describe('dogs', function () {
+    var dog;
+
+    beforeEach(function () {
+      dog = new Animal('dog');
+      // note, you could leave out this beforeEach and create the new animal inside each one of the it blocks instead. This way is just easier.
+    });
+
+    it('should bark if the animal is a dog', function () {
+      expect(dog.speak()).toEqual('bark');
+    });
+    it('shouldnt meow because its not a cat', function () {
+      expect(dog.speak()).not.toEqual('meow');
+    });
+  })
+
+  describe('cats', function () {
+    var cat;
+
+    beforeEach(function () {
+      cat = new Animal('cat');
+      // note, you could leave out this beforeEach and create the new animal inside each one of the it blocks instead. This way is just easier.
+    });
+
+    it('should meow if the animal is a cat', function () {
+      expect(cat.speak()).toEqual('meow');
+    });
+    it('should not bark because its not a dog', function () {
+      expect(cat.speak()).not.toEqual('bark');
+    });
+  })
+});
